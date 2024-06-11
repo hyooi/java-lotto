fd4f8b29de92c9d8ef89efa883ae8b11c5398a3d
hyoin <hyoin01@gmail.com>
2024-06-11 11:35:32 +0900

    refactor(ViewManager): ViewManager 및 ViewManager2 템플릿메소드로 통합

--- src/main/java/lotto/view/ViewManager.java
+++ src/main/java/lotto/view/ViewManager.java
@@ -2,25 +2,51 @@ package lotto.view;
 
 import lotto.entity.GameManager;
 import lotto.entity.LottoMachine;
+import lotto.entity.LottoNumber;
 
-public class ViewManager {
-    private final InputView inputView = new InputView();
-    private final ResultView resultView = new ResultView();
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ViewManager {
+    protected final InputView inputView = new InputView();
+    protected final ResultView resultView = new ResultView();
 
     public void start() {
         var amount = inputView.inputAmount();
+        var manualLottos = buyManualLotto();
 
         var lottoMachine = new LottoMachine();
-        var lottos = lottoMachine.buy(amount);
-        resultView.printPurchasedAutoLotto(lottos);
+        amount = lottoMachine.calculateRemainAmount(amount, manualLottos);
+
+        var autoLottos = lottoMachine.buy(amount);
+        printPurchasedLotto(manualLottos, autoLottos);
 
         var winningNumber = inputView.inputWinningNumber();
         var bonusNumber = inputView.inputBonusNumber();
 
         var gameManager = new GameManager();
-        var result = gameManager.getWinningDetails(winningNumber, bonusNumber, lottos);
+        var allLottos = mergeLottos(manualLottos, autoLottos);
+        var result = gameManager.getWinningDetails(winningNumber, bonusNumber, allLottos);
 
         resultView.printStatisticsResult(result);
         resultView.printProfitRate(gameManager.getProfitRate(amount, result));
+    }
+
+    private List<LottoNumber> mergeLottos(List<LottoNumber> manualLottos, List<LottoNumber> autoLottos) {
+        var result = new ArrayList<>(manualLottos);
+        result.addAll(autoLottos);
+
+        return result;
+    }
+
+    List<LottoNumber> buyManualLotto() {
+        return List.of();
+    }
+
+    void printPurchasedLotto(List<LottoNumber> manualLottos, List<LottoNumber> autoLottos) {
+        printPurchasedLotto0(autoLottos);
+    }
+
+    void printPurchasedLotto0(List<LottoNumber> autoLottos) {
     }
 }
